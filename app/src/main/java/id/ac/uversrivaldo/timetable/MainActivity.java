19a4f9c32c75922c9b8993b1e9366797168b3fcb
rivaldo30 <rivaldozhuo30@gmail.com>
2021-10-26 16:29:06 +0700

    commit 3

--- app/src/main/java/id/ac/uversrivaldo/timetable/MainActivity.java
+++ app/src/main/java/id/ac/uversrivaldo/timetable/MainActivity.java
@@ -2,11 +2,16 @@ package id.ac.uversrivaldo.timetable;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SeekBar;
+import android.widget.Toast;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
